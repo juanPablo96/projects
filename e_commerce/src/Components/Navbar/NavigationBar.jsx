99907888faf400c721c83b392f3a794e7ceb2ace
Juan Pablo <juanpablor1996@gmail.com>
2023-08-23 17:31:37 -0300

    styles in the NavigationBar Component

--- e_commerce/src/Components/Navbar/NavigationBar.jsx
+++ e_commerce/src/Components/Navbar/NavigationBar.jsx
@@ -1,34 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/images/logo.svg";
-import cart from "../../assets/icons/icon-cart.svg"
-import "./navigationBar.css"
+import cart from "../../assets/icons/icon-cart.svg";
+import "./navigationBar.css";
 
 // Boostrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 const NavigationBar = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then(async (res) => await res.json())
+      .then((res) => {
+        setUsers(res.results);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home" > <img src={logo} alt="Logo Sneakers" className="navBar_logo" /> </Navbar.Brand>
-          
+      <Navbar
+        bg="light"
+        data-bs-theme="light"
+        expand="lg"
+        style={{ width: "100%" }}
+      >
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo Sneakers" className="navBar_logo" />{" "}
+          </Navbar.Brand>
+
           <Nav className="me-auto nav_links">
-            <Nav.Link href="#" >Collections</Nav.Link>
+            <Nav.Link href="#">Collections</Nav.Link>
             <Nav.Link href="#">Men</Nav.Link>
             <Nav.Link href="#">Women</Nav.Link>
             <Nav.Link href="#">About</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
-            <Nav.Link href="#"><img src={cart} alt="" /></Nav.Link>
           </Nav>
-
+          <Navbar.Collapse className="justify-content-end">
+            <img src={cart} alt="" />
+            {users.map((user) => {
+              return(
+                
+                <img className="navbar_user" src={user.picture.thumbnail} alt="" />
+                
+              )
+              
+            })}
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-
-    
     </>
   );
 };
--- e_commerce/src/Pages/Header/Header.jsx
+++ e_commerce/src/Pages/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./header.css"
 import NavigationBar from "../../Components/Navbar/NavigationBar";
 
 const Header =()=>{
 
     return(
 
-        <>
+        <header className="header_navigation_bar">
         <NavigationBar/>
-        </>
+        </header>
     );
 }
 
